73e7e6db8993298cda0bf73ed7cbe8a96e69ef92
Ahmad Fadhel <ahmadfadhel1108@gmail.com>
2025-07-10 22:25:33 +0700

    debug: fixing 4.7

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const User = require("../models/User");
+const bcrypt = require("bcrypt")
 const generateToken = require("../utils/generateToken");
 
 // ===========================
